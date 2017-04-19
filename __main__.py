428205696944e4d96905e22a0b96a1c3b6d19f26
Dmitry Dolgopolov <dimafet@Dolgopolovs-MacBook-Pro.local>
2017-04-19 16:56:38 -0700

    Now it draws one face from 4 quite different angles

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 import numpy as numpy
 import matplotlib.pyplot as plt
-%matplotlib inline
+# %matplotlib inline
 
 
 # get a DB of the faces images
@@ -10,4 +10,16 @@ data = fetch_olivetti_faces().images
 
 
 # making a huge graph out of small graphs
-# plt.subplot()
+# plt.subplot(num of cells in height, num of cells in width, the # id of the cell)
+
+# Shows a face from 4 angles a bit inclined from straight
+plt.subplot(2, 2, 1)
+plt.imshow(data[0], cmap='gray')
+plt.subplot(2, 2, 2)
+plt.imshow(data[1], cmap='gray')
+plt.subplot(2, 2, 3)
+plt.imshow(data[2], cmap='gray')
+plt.subplot(2, 2, 4)
+plt.imshow(data[3], cmap='gray')
+
+plt.show()
