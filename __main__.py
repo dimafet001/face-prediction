217c33691bb6fce0707442916751810beab682d3
Dmitry Dolgopolov <dimafet@Dolgopolovs-MacBook-Pro.local>
2017-04-19 23:24:47 -0700

    shows a divided face plot

--- __main__.py
+++ __main__.py
@@ -14,19 +14,18 @@ data = fetch_olivetti_faces().images
 # plt.subplot(num of cells in height, num of cells in width, the # id of the cell)
 
 # Shows a face from 4 angles a bit inclined from straight
-for i in range(4):
-	plt.subplot(2, 2, i+1)
-	plt.imshow(data[i], cmap='gray')
-# plt.subplot(2, 2, 2)
-# plt.imshow(data[1], cmap='gray')
-# plt.subplot(2, 2, 3)
-# plt.imshow(data[2], cmap='gray')
-# plt.subplot(2, 2, 4)
-# plt.imshow(data[3], cmap='gray')
+# for i in range(4):
+	# plt.subplot(2, 2, i+1)
+	# plt.imshow(data[i], cmap='gray')
 
-plt.show()
+# plt.show()
 
 # cutting the left and right halves of the images
 X = data[:,:, : len(data[0][0])/2 ]
 Y = data[:,:,   len(data[0][0])/2 : ]
 
+plt.subplot(1, 2, 1)
+plt.imshow(X[0], cmap='gray')
+plt.subplot(1, 2, 2)
+plt.imshow(Y[0], cmap='gray')
+plt.show()
